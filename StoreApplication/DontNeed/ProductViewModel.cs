57f9583a7fb11efe51d4ecbbe460bae3fc787d7e
DanialBeh <120136265+DanialBeh@users.noreply.github.com>
2023-02-19 18:58:20 -0500

    Store view product

--- StoreApplication/DontNeed/ProductViewModel.cs
+++ StoreApplication/DontNeed/ProductViewModel.cs
@@ -4,11 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StoreApplication.ViewModels
+namespace StoreApplication.DontNeed
 {
-    internal class StoreViewModel
+    internal class ProductViewModel
     {
         public string VolleyballImg => "https://upload.wikimedia.org/wikipedia/commons/5/5a/Volleyball_ball.jpg";
-
     }
 }
