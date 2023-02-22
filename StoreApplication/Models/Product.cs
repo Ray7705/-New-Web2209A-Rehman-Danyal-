6ce8ff59790baddffe34899e0d20b5951bf53651
DanialBeh <120136265+DanialBeh@users.noreply.github.com>
2023-02-22 12:02:26 -0500

    Update Product.cs

--- StoreApplication/Models/Product.cs
+++ StoreApplication/Models/Product.cs
@@ -6,18 +6,39 @@ using System.Threading.Tasks;
 
 namespace StoreApplication.Models
 {
-    internal class Product
+    public  class Product
     {
-        public string? Name { get;private set; }
+        public Product(string name, int id, decimal price, string imgPath, double discount, string description)
+        {
+            if(string.IsNullOrEmpty(name))
+                throw new ArgumentNullException("Name must not be null");
+            if (string.IsNullOrEmpty(imgPath))
+                throw new ArgumentNullException("ImagePath must not be emthy");
+            if(id<0)
+                throw new ArgumentOutOfRangeException(nameof(id));
+            if(price<0)
+                throw new ArgumentOutOfRangeException(nameof(price));
+
+
+
+            Name = name;
+            Id = id;
+            Price = price;
+            ImgPath = imgPath;
+            Discount = discount;
+            Description = description;
+        }
+
+        public string Name { get;private set; }
         public int Id { get; }
         public decimal Price { get;private set; }
 
-        public string? ImgPath { get; private set; }
+        public string ImgPath { get; private set; }
 
         public double Discount { get; private set; } = 0;
 
 
-
+        public string Description { get; private set; }
 
 
     }
